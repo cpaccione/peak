a051e474dc372caf3545ca1da38377167b7b51c6
Chris Paccione <cpaccione77@gmail.com>
2020-05-19 19:25:48 -0500

    Lock s-foils in attack position...

---
+++ page-home.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+
+Template Name: Peak Home
+
+*/
+
+?>
+
+
+<?php get_header(); ?>
+
+    <div class="home-hero">
+        <div class="container-1000">
+            <div class="wrap">
+                <h2>Chicagoland Appliance Repair, Installation, and Maintenance</h2>
+                <h1>Heating, Cooling, <span class="no-wrap">&amp; Refrigeration</span></h1>
+                <a class="button" href="">schedule service</a>
+                <a class="phone" href="tel:8475108302">or Call 847-510-8302</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="home-services">
+        <div class="container-1000">
+            <h2>appliance repair services</h2>
+            <p>commercial &amp; residential</p>
+            <a href="" class="button">learn more</a>
+
+            <div class="service-grid">
+                <div class="icon">
+                    <img src="<?php echo esc_url(get_template_directory_uri() );?>/images/temp_icon.svg" alt="icon">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<?php get_footer(); ?>

c591f686e1968900ca9010fd6963f95d3bd91d6a
Chris Paccione <cpaccione77@gmail.com>
2020-06-07 09:32:59 -0500

    Started modal work

--- template-parts/blocks/appliance_repeater/appliance_repeater.php
+++ template-parts/blocks/appliance_repeater/appliance_repeater.php
@@ -50,12 +50,13 @@ $link = get_field('link');
 
                     ?>
 
-                    <div class="icon-wrap">
-                        <div class="icon">
-                            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+                        <div class="icon-wrap myBtn">
+                            <div class="icon">
+                                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+                            </div>
+                            <p><?php echo $service; ?></p>
                         </div>
-                        <p><?php echo $service; ?></p>
-                    </div>
+                    
 
                 <?php endwhile; ?>
 
@@ -78,3 +79,44 @@ $link = get_field('link');
 
     </div>
 </div>
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <p>Some text in the Modal..</p>
+  </div>
+
+</div>
+
+
+<script>
+
+    // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.querySelector(".myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
